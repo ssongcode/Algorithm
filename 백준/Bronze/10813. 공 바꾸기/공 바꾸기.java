157123e3ae93591c347b37bd71a817adf123400a
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2023-06-15 22:46:05 +0900

    [Bronze II] Title: 공 바꾸기, Time: 76 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	static BufferedReader br;
+
+	static String endl = "\n";
+	static String blank = " ";
+
+	static int N, M;
+
+	static void input() throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		stk();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+	}
+
+	static void pro() throws IOException {
+		int[] arr = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			arr[i] = i;
+		}
+		int a, b, tmp;
+		for (int i = 0; i < M; i++) {
+			stk();
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			tmp = arr[a];
+			arr[a] = arr[b];
+			arr[b] = tmp;
+		}
+		for (int i = 1; i <= N; i++) {
+			sb.append(arr[i]).append(blank);
+		}
+		System.out.println(sb);
+	}
+
+	public static void main(String[] args) throws IOException {
+		input();
+		pro();
+	}
+
+	static void stk() throws IOException {
+		st = new StringTokenizer(br.readLine());
+	}
+
+}
