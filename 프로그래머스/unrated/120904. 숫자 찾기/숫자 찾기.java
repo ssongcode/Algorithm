3fdc3673e4d88b5620821c560b80180acbf1bd03
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2023-10-15 22:57:20 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num, int k) {
+        String str = String.valueOf(num);
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == (char)k+'0')
+                return i+1;
+        }
+        return -1;
+    }
+}
