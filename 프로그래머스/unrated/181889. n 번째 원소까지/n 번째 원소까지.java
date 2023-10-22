235aa5d3c9f16346de263f09a0676f6a3ecff615
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2023-10-23 00:36:44 +0900

    [level unrated] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num, int n) {
+        int len = n;
+        int[] answer = new int[len];
+        for(int i = 0; i < len; i++) {
+            answer[i] = num[i];
+        }
+        
+        return answer;
+    }
+}
