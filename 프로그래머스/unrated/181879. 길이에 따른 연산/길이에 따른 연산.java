f9a433b9453e6703ada15227ad960522eae56cb7
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2023-10-30 00:18:48 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int len = num_list.length;
+        int answer = 0;
+        if(len >= 11) {
+            for(int i = 0; i< len; i++) {
+                answer += num_list[i];
+            }
+        }
+        else {
+            answer = 1;
+            for(int i = 0; i< len; i++) {
+                answer *= num_list[i];
+            }
+        }
+        return answer;
+    }
+}
