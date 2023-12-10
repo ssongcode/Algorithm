8c75c7f70fdad4d80792e48be5b8505775ff99f8
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2023-12-10 22:13:32 +0900

    [level unrated] Title: 소문자로 바꾸기, Time: 0.15 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        answer = myString.toLowerCase();
+        return answer;
+    }
+}
